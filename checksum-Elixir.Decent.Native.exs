de83e219c527cde6ad2518d6ed024a8fd841f40b
Mayel de Borniol <git@mayel.space>
2026-06-01 15:51:27 +0100

    Add checksum file for v0.2.0 Linux targets

---
+++ checksum-Elixir.Decent.Native.exs
@@ -0,0 +1,7 @@
+%{
+  "libdecent-v0.2.0-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:9c1818f5b0a54ebfbf3f587ff0c62186daa382af33be2c563f47e3524e664da7",
+  "libdecent-v0.2.0-nif-2.17-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:998f9f9fbb914a70ba58b00e9ba783914479afaf2256cbf7ee0f88168938c742",
+  "libdecent-v0.2.0-nif-2.17-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:8e42ee6cb18da897fd5410235d04cba4e13b379273b6e83e53921564caf7d4da",
+  "libdecent-v0.2.0-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:36096fda21c9bb081bd685a0d10f457fd6d64509e1274951508ac9db1e53aa78",
+  "libdecent-v0.2.0-nif-2.17-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:ae8df07fdf37abbb2216a8f3de80a308157b4f117e8f9c8d91382d5eb7b72e30",
+}
